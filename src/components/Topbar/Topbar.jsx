dafdd5ef63ce506f0bbc40272005347e73627f06
Adhithyan Pradeep <adhithyanpradeep666@gmail.com>
2026-06-16 23:22:58 +0530

    chore: remove text and beta badge from logo

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -25,8 +25,6 @@ export default function Topbar({ onRun, onOpenPackages, isRunning, pyodideReady,
       {/* Logo */}
       <div className="topbar-logo">
         <span className="topbar-logo-icon">🍍</span>
-        <span className="topbar-logo-text">Pynapple</span>
-        <span className="topbar-logo-badge">BETA</span>
       </div>
 
       {/* Center: loading status */}
